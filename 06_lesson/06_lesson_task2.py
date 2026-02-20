912d7b25e2ebf46dfb9804a4717e2b6792667a44
Darya198 <kazhalova2018@yandex.ru>
2026-02-20 16:09:40 +0300

    Исправила ошибки

--- 06_lesson/06_lesson_task2.py
+++ 06_lesson/06_lesson_task2.py
@@ -2,15 +2,18 @@ from selenium import webdriver
 from selenium.webdriver.chrome.service import Service as ChromeService
 from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.common.by import By
-
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 
 service = ChromeService(ChromeDriverManager().install())
 driver = webdriver.Chrome(service=service)
+wait = WebDriverWait(driver, 10)
 
 driver.get("http://uitestingplayground.com/textinput")
 
-form = driver.find_element(By.CSS_SELECTOR, "#newButtonName")
-form.clear
+form = wait.until(EC.element_to_be_clickable(
+    (By.CSS_SELECTOR, "#newButtonName")))
+form.clear()
 form.send_keys('SkyPro')
 
 button = driver.find_element(By.CSS_SELECTOR, "#updatingButton")
