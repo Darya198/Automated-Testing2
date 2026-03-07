0319f349518eef7ed4529752460ab322928dd523
Darya198 <kazhalova2018@yandex.ru>
2026-03-07 20:18:35 +0300

    редактировала

--- 09_lesson/confing_file.py
+++ 09_lesson/confing_file.py
@@ -1,13 +1,14 @@
-from sqlalchemy import create_engine 
+from sqlalchemy import create_engine
 
 
 db_user = "postgres"
-db_pass = "mnb098890bnm" 
+db_pass = "mnb098890bnm"
 db_host = "localhost"
 db_port = "5432"
 db_name = "QA2"
 
-database_url = f"postgresql://{db_user}:{db_pass}@{db_host}:{db_port}/{db_name}"
+database_url = f"postgresql://{db_user}:{
+    db_pass}@{db_host}:{db_port}/{db_name}"
 
 db = create_engine(database_url)
 
@@ -18,7 +19,10 @@ create table if not exists students (
     user_age int
 );
 """
-sql_insert = "INSERT INTO students (user_name, user_age) VALUES (:name, :age) RETURNING user_id;"
+sql_insert = """
+INSERT INTO students (user_name, user_age)
+VALUES (:name, :age) RETURNING user_id;
+"""
 sql_select_id = "SELECT * FROM students WHERE user_id = :id;"
 sql_update = "UPDATE students SET user_name = :name WHERE user_id = :id;"
 sql_delete = "DELETE FROM students WHERE user_id = :id;"
